26b48769a287a7b9039691a70e9a34778f9e6b47
Denis Zablotsky <deniszablotsky@proton.me>
2026-02-02 23:08:08 -0500

    Fix link

--- app/me/route.ts
+++ app/me/route.ts
@@ -4,9 +4,9 @@ import { supabase } from "../supabase";
 export async function GET() {
 const session = await supabase.auth.getSession();
 const accessToken = session.data.session?.access_token;
-const backendUrl = process.env.BACKEND_URL;
+const link = process.env.BACKEND_URL + "/me";
 
-const res = await fetch(`${backendUrl}/me`, {
+const res = await fetch(link, {
   headers: {
     Authorization: `Bearer ${accessToken}`,
   },
